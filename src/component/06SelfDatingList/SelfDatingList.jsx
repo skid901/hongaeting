c10dcdd1557c518f73b81aae800ab20b95764b79
outwater <kocon135@naver.com>
2019-10-03 12:20:08 +0900

    셀소디테일 1차 구현

--- src/component/06SelfDatingList/SelfDatingList.jsx
+++ src/component/06SelfDatingList/SelfDatingList.jsx
@@ -1,37 +1,40 @@
-import React, {useState, useEffect} from 'react';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import MaterialTable from 'material-table';
-import menu from './menu.png'
-import send from './send.png'
-
+import React, { useState, useEffect } from 'react';
 import './SelfDatingList.scss';
 import Axios from '../../../node_modules/axios/index';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Paper from '@material-ui/core/Paper';
+import MaterialTable from 'material-table';
 
 const SelfDatingList = () =>{
-  const [columns, setColumns] = useState([
-    {title : 'ID', field : 'id'},
-    {title : '이름', field : 'name'},
-    {title : '나이', field : 'age'},
-    {title : '소속', field : 'nationality'},
-    {title : '키워드1', field : 'tag1'},
-    {title : '키워드2', field : 'tag2'},
-    {title : '키워드3', field : 'tag3'},
+  const [columns, setColumns ] = useState([
+    {title :  '키워드4', field : 'tag4'},
+    {title : 'ID' , field : 'id' },
+    {title :  '닉네임', field :'name' },
+    {title :  '나이', field : 'age' },
+    {title :  '소속', field : 'nationality' },
+    {title :  '키워드1', field : 'tag1' },
+    {title :  '키워드2', field : 'tag2'},
+    {title :  '키워드3', field : 'tag3'},
+
   ])
+
   const [datas, setdatas] = useState([
     {
+      tag4: '',
       id:'0',
-      name:'Demo',
-      age:'Demo',
-      nationality:'Demo',
-      introduction: '',
-      tag1:'Demo',
-      tag2:'Demo',
-      tag3:'Demo'
+      name:'',
+      email:'',
+      age:'',
+      nationality:'',
+      tag1:'',
+      tag2:'',
+      tag3:'',
     }
   ])
   useEffect(()=>{
@@ -39,29 +42,31 @@ const SelfDatingList = () =>{
   },[])
 
   const getTableData = async () => {
-    try{
-      return await Axios.get('https://spreadsheets.google.com/feeds/list/1xOsFfS4CuvEMxssqDOKlrxzXMW7jxiuH-49Pq9svGg4/1/public/full?alt=json');
+    try{  //what is the try?
+      return await Axios.get('https://spreadsheets.google.com/feeds/list/1MJ6UiMywoga78H2EHQ70V0TP9I_v1dyx6VUCqUs6RCY/1/public/full?alt=json')
     } catch (error){
       console.error(error);
     }
   }
-
+  
+  
   const useTableData = async() => {
     const TableDatas = await getTableData();
-    const a = await TableDatas.data.feed.entry;
-    const length = a.length;
-    let tmplist=[];
+    const row = await TableDatas.data.feed.entry;
+    const length = row.length;
+    let tmplist=[]; // what is the tmp?
     let tmp = {
-      id: '0',
-      name: 'Demo',
-      age: 'Demo',
-      nationality: 'Demo',
-      introduction: 'Demo',
-      tag1: 'Demo',
-      tag2: 'Demo',
-      tag3: 'Demo'
+      id:'0',
+      name:'',
+      email:'',
+      age:'',
+      nationality:'',
+      tag1:'',
+      tag2:'',
+      tag3:'',
+      tag4:''
     };
-    for(let i = 0; i < length ; i++){
+    for(let i= 0; i<length; i++){
       tmp.id = TableDatas.data.feed.entry[i].gsx$id.$t;
       tmp.name = TableDatas.data.feed.entry[i].gsx$name.$t;
       tmp.age = TableDatas.data.feed.entry[i].gsx$age.$t;
@@ -71,7 +76,7 @@ const SelfDatingList = () =>{
       tmp.tag2 = TableDatas.data.feed.entry[i].gsx$tag2.$t;
       tmp.tag3 = TableDatas.data.feed.entry[i].gsx$tag3.$t;
       tmplist.push({
-        id : tmp.id,
+        id:tmp.id,
         name: tmp.name,
         age: tmp.age,
         nationality: tmp.nationality,
@@ -79,42 +84,49 @@ const SelfDatingList = () =>{
         tag1: tmp.tag1,
         tag2: tmp.tag2,
         tag3: tmp.tag3,
+        tag4: `(${tmp.id})${tmp.age}/${tmp.name}`
       });
     }
-    setdatas(tmplist);
+    setdatas(tmplist); 
   }
 
-  return(
-    <div className = "Template" sytle={"width : 720px", "margin : 0 auto"}>
-      <div className="header">
-        <img src={menu} alt="" className = "Menu" />
-        <div className = "Title">홍셀소</div>
-        <img src={send} alt="" className = "Send" />
-      </div>
-      <div className="pagename">
-        <div className="pagename1">
-          홍익 셀프 소개팅
-        </div>
-        <div className="pagename2">
-          여러 학우들과의 대화와 만남
-        </div>
-        <button
-          className="apply"
-        >
-          홍셀소 신청하기
-        </button>
-      </div>
-      <div className="body">
-        <div>
-          남학우_셀소
-        </div>
-        <div>
-          1.<br/>
-          2.<br/>
-          3.<br/>
-        </div>
-      </div>
-      <Paper>
+  const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+  const classes = useStyles();
+
+  console.log(datas[0].id)
+  return (
+     <div>
+       <div className="Head">
+       <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                홍셀소
+              </Typography>
+              <Button color="inherit">Send</Button>
+            </Toolbar>
+          </AppBar>
+         </div>
+         <h1 className="imoji">&nbsp;😊남학우_셀소</h1>
+         <p>&nbsp; 1.자기소개 검색이 가능해요.</p>
+         <p>&nbsp; 2.나이/성별을 클릭하면 더 자세한 소개를 볼 수 있어요.</p>
+         <p className="star">&nbsp;*개인정보 노출 최소화를 위해 단과대학은 올려드리지 않아요</p>
+         <p>&nbsp; 3. 검색 키워드 :  큰 키, 고양이상, 이목구비 뚜y렷, 혼혈느낌, 귀여운, 동안, 쌍커풀, 예쁨, 다정, 외향, 리액션, 사교적, 배려심, 웃음기, 장난, 맛집탐방, 독서, 수영, 전시회, 한강</p>
+       </div>
+       <Paper>
         <MaterialTable
           title="셀소"
           columns={columns}
@@ -125,10 +137,22 @@ const SelfDatingList = () =>{
           }
         />
       </Paper>
-
-      
-    </div>
-  )  
-}
-
+{/* <div id="user-table">
+          <Table>
+            <TableHead>
+// // // //              <TableRow>
+// // // //                <TableCell>나이/성별</TableCell>
+// // // //                <TableCell>자기소개</TableCell>
+// // // //              </TableRow>
+// // // //            </TableHead>
+// // // //            <TableBody>
+// // // //              {datas.map(obj=>{
+// // // //                  return <User key={obj.email} hash={`${obj.tag1} ${obj.tag2} ${obj.tag3}`}  info={obj.sex} />
+// // // //                })}
+// // // //            </TableBody>
+// // // //          </Table>
+// // // //        </div> */}
+     </div>
+   );
+ }
 export default SelfDatingList;
--- src/component/07SelfDatingDetails/SelfDatingDetails.jsx
+++ src/component/07SelfDatingDetails/SelfDatingDetails.jsx
@@ -1,80 +1,196 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './SelfDatingDetails.scss';
-import Tabletop from 'tabletop';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
+
+import Axios from '../../../node_modules/axios/index';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Paper from '@material-ui/core/Paper'
+import CommentIcon from '@material-ui/icons/Comment';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import User from './User'
-import ButtonAppBar from'./ex';
-import ExpansionBar from './ExtentionBar';
+import Table from '@material-ui/core/Table';
 
-class Sheet extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [],
+const SelfDatingDetails = () =>{
+  const [datas, setdatas] = useState([
+    {
+      id:'',
+      name:'',
+      email:'',
+      age:'',
+      sex:'',
+      nationality:'',
+      tag1:'',
+      tag2:'',
+      tag3:''
+    }
+  ])
+  useEffect(()=>{
+    useTableData();
+  },[])
+
+  const getTableData = async () => {
+    try{  //what is the try?
+      return await Axios.get('https://spreadsheets.google.com/feeds/list/1MJ6UiMywoga78H2EHQ70V0TP9I_v1dyx6VUCqUs6RCY/1/public/full?alt=json')
+    } catch (error){
+      console.error(error);
+    }
+  }
+  
+  
+  const useTableData = async() => {
+    const TableDatas = await getTableData();
+    const row = await TableDatas.data.feed.entry;
+    const length = row.length;
+    let tmplist=[]; // what is the tmp?
+    let tmp = {
+      id:'0',
+      name:'',
+      email:'',
+      age:'',
+      sex:'',
+      nationality:'',
+      tag1:'',
+      tag2:'',
+      tag3:'',
+      link:''
     };
+    for(let i= 0; i<length; i++){
+      tmp.id = TableDatas.data.feed.entry[i].gsx$id.$t;
+      tmp.name = TableDatas.data.feed.entry[i].gsx$name.$t;
+      tmp.age = TableDatas.data.feed.entry[i].gsx$age.$t;
+      tmp.sex = TableDatas.data.feed.entry[i].gsx$sex.$t;
+      tmp.email = TableDatas.data.feed.entry[i].gsx$email.$t;   
+      tmp.nationality = TableDatas.data.feed.entry[i].gsx$nationality.$t;
+      tmp.introduction = TableDatas.data.feed.entry[i].gsx$introduction.$t;
+      tmp.tag1 = TableDatas.data.feed.entry[i].gsx$tag1.$t;
+      tmp.tag2 = TableDatas.data.feed.entry[i].gsx$tag2.$t;
+      tmp.tag3 = TableDatas.data.feed.entry[i].gsx$tag3.$t;
+      tmp.link = TableDatas.data.feed.entry[i].gsx$link.$t;
+      tmplist.push({
+        id:tmp.id,
+        name: tmp.name,
+        age: tmp.age,
+        sex: tmp.sex,
+        email: tmp.email,
+        nationality: tmp.nationality,
+        introduction: tmp.introduction,
+        tag1: tmp.tag1,
+        tag2: tmp.tag2,
+        tag3: tmp.tag3,
+        link: tmp.link,
+      });
+    }
+    setdatas(tmplist); 
   }
 
-  componentDidMount() {
-    Tabletop.init({
-      key: '1MJ6UiMywoga78H2EHQ70V0TP9I_v1dyx6VUCqUs6RCY',
-      callback: googleData => {
-        this.setState({
-          data: googleData,
-        });
-      },
-      simpleSheet: true,
-    });
-  }
+  const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    Expansion: {
+      width: '100%',
+    },
+    Expansionhead: {
+      fontSize: theme.typography.pxToRem(18),
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+    root1: {
+      width: '100%',
+      maxWidth: 3000,
+      backgroundColor: theme.palette.background.paper,
+    },
+    table: {
+      minWidth: 100,
+      maxWidth: 500
+    },
+  }));
+  const classes = useStyles();
 
-  render() {
-    console.log('updated state--->', this.state);
-    const { data } = this.state;
-    console.dir(data)
-    console.dir(data[0])
-    console.log(data)
-    console.log(data[0])
-    console.log();
+   console.log(datas[0])
+   console.log(datas[0].email)
     return (
       <div>
-        <div>
-          <div>
-            <ButtonAppBar />
-          </div>
-          <h1 className="imoji">😊</h1>
-          <h1>(532)24/남학우(..HardCoding..ㅠ)</h1>
-          <h1>밑부분 고장..</h1>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                  홍셀소
+              </Typography>
+              <Button color="inherit">Send</Button>
+            </Toolbar>
+          </AppBar>
         </div>
-        <div id="user-table">
+        <h1 className="imoji">😊</h1>
+        <h1>({datas[0].id}) {datas[0].age} / {datas[0].sex}</h1>
+        <Paper>
           <Table>
-            <TableHead>
+            <TableBody>    
               <TableRow>
-                <TableCell>Hash</TableCell>
-                <TableCell>Email</TableCell>
-                <TableCell>Link</TableCell>
-                <TableCell>Date</TableCell>
+                <TableCell variant="head"><CommentIcon /> 자기소개</TableCell>
+                <TableCell> {datas[0].tag1}</TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map(obj=>{
-                  return <User key={obj.email} hash={`${obj.hash1} ${obj.hash2} ${obj.hash3}`} email={obj.email} link={obj.link} date={obj.date} />
-                })}
+              <TableRow>    
+                <TableCell variant="head"><CommentIcon />링크</TableCell>
+                <TableCell><a href={datas[0].link}>{datas[0].link}</a></TableCell>
+              </TableRow>
             </TableBody>
           </Table>
-        </div>    
-        <div className="Sheet">
-          <ExpansionBar name={data.map(obj=>{
-                  return <User key={obj.content1} content1={obj.content1} content2={obj.content2} />
-                })}
-          />
+        </Paper>       
+        <div className={classes.Expansion}>
+          <ExpansionPanel>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+            <Typography className={classes.Expansiobhead}>😊외모 </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>그의 외모는 = {datas[0].tag1}</Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+            <Typography className={classes.heading}>😊성격 </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>그의 성격은 = {datas[0].tag2}</Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel disabled>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3a-content"
+              id="panel3a-header"
+            >
+            <Typography className={classes.heading}>Disabled Expansion Panel</Typography>
+            </ExpansionPanelSummary>
+          </ExpansionPanel>
         </div>
       </div>
-
-    );
+    )
   }
-}
-
-export default Sheet;
+export default SelfDatingDetails;
