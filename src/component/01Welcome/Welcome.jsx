4bced4924c4c65626f0faead05a4abf2381a2554
10000001a <pgdn06288888@gmail.com>
2019-10-11 21:12:15 +0900

    welcome start

--- src/component/01Welcome/Welcome.jsx
+++ src/component/01Welcome/Welcome.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Contents1 from './welcomeContents1.jpg';
 
 import './Welcome.scss';
 
-const Welcome = () => <>Welcome Page will be here...</>;
+const Welcome = () => {
+  return (
+    <div className="Template">
+      <div className="Header">
+        <div>
+          홍개팅
+        </div>
+        <button>
+          홍개팅 이용하기
+        </button>
+      </div>
+      <img src={Contents1} alt={'a'} />
+    </div>
+  )
+}
 
 export default Welcome;
