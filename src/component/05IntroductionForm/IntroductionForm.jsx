38b1f336b71aaf32440a39460b32353f3366702f
skid901 <tlrudejr1993@naver.com>
2019-10-18 13:15:53 +0900

    temporarily make iframe

--- src/component/05IntroductionForm/IntroductionForm.jsx
+++ src/component/05IntroductionForm/IntroductionForm.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './IntroductionForm.scss';
 
-const IntroductionForm = () => <>IntroductionForm will be here...</>;
+function IntroductionForm() {
+  return (
+    <div className="intriduction-form">
+      <div className="comment">
+        {`설문지 제출 완료 후 `}
+        <Link to="/selfdatinglist">셀소 리스트</Link>
+        {`로 이동`}
+      </div>
+      <div className="iframe-wrapper">
+        <iframe
+          title="introduction-form"
+          src="https://docs.google.com/forms/d/e/1FAIpQLSe3ZU06TvwUbDmNaFiP-4SxMMRFtuJUWY_HYRM2KRFqSUacfg/viewform?embedded=true"
+          frameBorder="0"
+          marginHeight="0"
+          marginWidth="0"
+        >
+          로드 중…
+        </iframe>
+      </div>
+    </div>
+  );
+}
 
 export default IntroductionForm;
