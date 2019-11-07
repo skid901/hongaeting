3461b0b3869455ec467afed7781aa3827423edd3
10000001a <pgdn06288888@gmail.com>
2019-11-07 22:50:51 +0900

    fix create

--- server/api/datingUsers/controller.js
+++ server/api/datingUsers/controller.js
@@ -36,32 +36,31 @@ export const create = async (ctx) => {
 */
 export const create = async (ctx) => {
   
-  let id = 2;
-  let time = "2019-11-04T06:50:31.403Z";
-  let email = "pgdn06288888@gmail.com";
-  let address = "010-2851-3468";
-  let gender = "남학우";
-  let age = "22세";
-  let collage = "건축대학";
-  let self = 0;
-  let same = 0;
-  let appearance = "매우매우매우매우매우잘생김";
-  let personality = "성격성격성격성격성격성격성격";
-  let hobby = "춤취미취미취미치미취미취미취미취미";
-  let religion = "무교";
-  let smoke = "비흡연자";
-  let idealtype = "이상형이상형이상형이상형이상형이상형";
-  let chatlink = "linklinklinklinklinklinn";
-  let tag = "#ㅁㅁㅁ#ㅠㅠㅠ#vvv#sjgl#태그";
-  let keysentence = "한마디두마디세마디네마디";
-  let appearance2 = "demo";
-  let personality2 = "demo";
-  let hobby2 = "demo";
-  let idealtype2 = "demo";
-  let religion2 = "demo";
-  let smoke2 = "demo";
+  // let id = 2;
+  // let time = "2019-11-04T06:50:31.403Z";
+  // let email = "pgdn06288888@gmail.com";
+  // let address = "010-2851-3468";
+  // let gender = "남학우";
+  // let age = "22세";
+  // let collage = "건축대학";
+  // let self = 0;
+  // let same = 0;
+  // let appearance = "매우매우매우매우매우잘생김";
+  // let personality = "성격성격성격성격성격성격성격";
+  // let hobby = "춤취미취미취미치미취미취미취미취미";
+  // let religion = "무교";
+  // let smoke = "비흡연자";
+  // let idealtype = "이상형이상형이상형이상형이상형이상형";
+  // let chatlink = "linklinklinklinklinklinn";
+  // let tag = "#ㅁㅁㅁ#ㅠㅠㅠ#vvv#sjgl#태그";
+  // let keysentence = "한마디두마디세마디네마디";
+  // let appearance2 = "demo";
+  // let personality2 = "demo";
+  // let hobby2 = "demo";
+  // let idealtype2 = "demo";
+  // let religion2 = "demo";
+  // let smoke2 = "demo";
   
-  /*
   const {
     id,
     time,
@@ -88,7 +87,7 @@ export const create = async (ctx) => {
     religion2,
     smoke2
   } = ctx.request.body;
-  */
+  
   const datingUser = new DatingUser({
     id,
     time,
