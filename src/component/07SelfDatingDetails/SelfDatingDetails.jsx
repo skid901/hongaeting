79ac2033fd052648c784eb47c5f5bb1c07fbe699
10000001a <pgdn06288888@gmail.com>
2019-11-20 16:04:14 +0900

    3

--- src/component/07SelfDatingDetails/SelfDatingDetails.jsx
+++ src/component/07SelfDatingDetails/SelfDatingDetails.jsx
@@ -300,7 +300,7 @@ const SelfDatingDetails = ({ selectedUser }) => {
             </ExpansionPanelSummary>
             <ExpansionPanelDetails>
               <Typography style={{ 'font-family': 'Noto Sans KR, sans-serif' }}>
-                {selectedUser.idealtype.replace('\n', '\\r\\n')}
+                {selectedUser.idealtype.replace('\n', 'EnterPlease')}
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
