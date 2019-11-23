f1cd6d19b9cd6001ce85ae29020205ec302d5a53
10000001a <pgdn06288888@gmail.com>
2019-11-23 16:33:13 +0900

    self prob 2

--- server/api/datingUsers/controller.js
+++ server/api/datingUsers/controller.js
@@ -65,8 +65,10 @@ export const create = async (ctx) => {
   if(email === '' || address === '' || gender === '' || age === '' || collage === ''){
     id = id.toString();
     try{
-      if (self==0) {await DatingUser.update({id : id},{self : self}); console.log("1");} else{console.log("2");}
-      if (same==0) {await DatingUser.update({id: id },{same : same}); console.log("1");} else{console.log("2");}
+      if (self==0) {await DatingUser.update({id : id},{self : 0}); console.log("1");} else{console.log("2");}
+      else if(self ==1) {await DatingUser.update({id : id},{self : 1}); console.log("1");} else{console.log("2");}
+      if (same==0) {await DatingUser.update({id: id },{same : 0}); console.log("1");} else{console.log("2");}
+      else if(same ==1) {await DatingUser.update({id : id},{same : 1}); console.log("1");} else{console.log("2");}
       if (appearance) {await DatingUser.update({id : id},{appearance : appearance}); console.log("1");} else{console.log("2");}
       if (personality) {await DatingUser.update({id: id},{personality : personality}); console.log("1");}else{console.log("2");}
       if (hobby) {await DatingUser.update({id : id},{hobby : hobby}); console.log("1");}else{console.log("2");}
